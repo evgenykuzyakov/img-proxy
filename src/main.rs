c8114bc0f0a668e511cfd2d834dc0d5bba5af8b9
Evgeny Kuzyakov <ek@nearprotocol.com>
2023-08-07 14:39:36 -0700

    0.6.0: Support magic text resolver for better cached previews

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,7 @@ pub enum FetchError {
     RequestFailed,
     UnsupportedContentType,
     BodyReadFailed,
+    TextReadFailed,
     Status(StatusCode),
 }
 
@@ -47,8 +48,20 @@ pub enum CachedImage {
     },
 }
 
+#[derive(Debug, Clone)]
+pub enum CachedMagicUrl {
+    Failed {
+        err: FetchError,
+        attempts: Vec<DateTime<Utc>>,
+    },
+    Success {
+        url: String,
+        time: DateTime<Utc>,
+    },
+}
 
 type ImgPair = (ImgType, String);
+type MagicCache = Arc<Mutex<HashMap<String, CachedMagicUrl>>>;
 type ImgCache = Arc<Mutex<HashMap<ImgPair, CachedImage>>>;
 
 #[derive(Debug, Clone, BorshSerialize, BorshDeserialize)]
@@ -65,47 +78,55 @@ pub fn sha256(data: &[u8]) -> [u8; 32] {
 
 #[tokio::main]
 async fn main() {
-    if env::var_os("IMAGE_RESCALE_URL_Thumbnail").is_none() || env::var_os("IMAGE_RESCALE_URL_Large").is_none() {
+    if env::var_os("IMAGE_RESCALE_URL_Thumbnail").is_none()
+        || env::var_os("IMAGE_RESCALE_URL_Large").is_none()
+    {
         panic!("Env IMAGE_RESCALE_URL_Thumbnail and IMAGE_RESCALE_URL_Large are required");
     }
     env_logger::init();
 
-    // Keep track of all connected users, key is usize, value
-    // is an event stream sender.
     let imgs: ImgCache = Arc::new(Mutex::new(HashMap::new()));
-    // Turn our "state" into a new Filter...
     let imgs = warp::any().map(move || imgs.clone());
+
+    let magic: MagicCache = Arc::new(Mutex::new(HashMap::new()));
+    let magic = warp::any().map(move || magic.clone());
 
     let cors = warp::cors().allow_any_origin();
     let log = warp::log("warp");
 
-    let proxy = warp::path!(String / ..)
-        .and(warp::path::tail())
-        .and(
-            warp::filters::query::raw()
-                .map(|q| Some(q))
-                .or(warp::any().map(|| None))
-                .unify(),
-        )
-        .and(imgs)
-        .and_then(
-            |img_type, img_path: Tail, query: Option<String>, imgs: ImgCache| async move {
-                let url = if let Some(query) = query {
-                    format!("{}?{}", img_path.as_str(), query)
-                } else {
-                    img_path.as_str().to_string()
-                };
-                match proxy_img(img_type, url, imgs).await {
-                    Ok(Image { content_type, body }) => Ok(Response::builder()
-                        .header("content-type", content_type)
-                        .header("Cache-Control", "public,max-age=2592000")
-                        .body(body)),
-                    Err(_e) => Err(warp::reject::reject()),
-                }
-            },
-        )
-        .with(cors.clone())
-        .with(log);
+    let proxy =
+        warp::path!(String / ..)
+            .and(warp::path::tail())
+            .and(
+                warp::filters::query::raw()
+                    .map(|q| Some(q))
+                    .or(warp::any().map(|| None))
+                    .unify(),
+            )
+            .and(imgs)
+            .and(magic)
+            .and_then(
+                |img_type,
+                 img_path: Tail,
+                 query: Option<String>,
+                 imgs: ImgCache,
+                 magic: MagicCache| async move {
+                    let url = if let Some(query) = query {
+                        format!("{}?{}", img_path.as_str(), query)
+                    } else {
+                        img_path.as_str().to_string()
+                    };
+                    match proxy_img(img_type, url, imgs, magic).await {
+                        Ok(Image { content_type, body }) => Ok(Response::builder()
+                            .header("content-type", content_type)
+                            .header("Cache-Control", "public,max-age=2592000")
+                            .body(body)),
+                        Err(_e) => Err(warp::reject::reject()),
+                    }
+                },
+            )
+            .with(cors.clone())
+            .with(log);
 
     let port: u16 = env::var_os("PORT")
         .map(|port| port.to_str().unwrap().parse().unwrap())
@@ -114,12 +135,30 @@ async fn main() {
     warp::serve(proxy).run(([127, 0, 0, 1], port)).await;
 }
 
-async fn proxy_img(img_type: String, url: String, imgs: ImgCache) -> Result<Image, FetchError> {
+async fn proxy_img(
+    mut img_type: String,
+    mut url: String,
+    imgs: ImgCache,
+    magic: MagicCache,
+) -> Result<Image, FetchError> {
+    let is_magic = img_type == "magic";
+    if is_magic {
+        if let Some((t, u)) = url.clone().split_once("/") {
+            img_type = t.to_string();
+            url = u.to_string();
+        } else {
+            return Err(FetchError::InvalidRescaleType);
+        }
+    }
     let img_type = match img_type.as_str() {
         "thumbnail" => ImgType::Thumbnail,
         "large" => ImgType::Large,
         _ => return Err(FetchError::InvalidRescaleType),
     };
+    if is_magic {
+        url = resolve_magic_url(url, magic).await?;
+    }
+
     let pair = (img_type, url);
     let img = imgs.lock().unwrap().get(&pair).cloned();
     let mut attempts = if let Some(img) = img {
@@ -139,7 +178,6 @@ async fn proxy_img(img_type: String, url: String, imgs: ImgCache) -> Result<Imag
             CachedImage::Success { image, .. } => return Ok(image),
         }
     } else {
-        // TODO: read from disk
         if let Some(saved_image) = read_from_disk(&pair) {
             info!(target: "cache", "Retrieving from disk {:?} {}", pair.0, pair.1);
             return Ok(cache_and_return(imgs, saved_image));
@@ -148,7 +186,10 @@ async fn proxy_img(img_type: String, url: String, imgs: ImgCache) -> Result<Imag
     };
     let url = format!(
         "{}/{}",
-        env::var_os(format!("IMAGE_RESCALE_URL_{:?}", pair.0)).unwrap().to_str().unwrap(),
+        env::var_os(format!("IMAGE_RESCALE_URL_{:?}", pair.0))
+            .unwrap()
+            .to_str()
+            .unwrap(),
         pair.1
     );
     let res = fetch_img(url).await;
@@ -172,8 +213,61 @@ async fn proxy_img(img_type: String, url: String, imgs: ImgCache) -> Result<Imag
     }
 }
 
+async fn resolve_magic_url(url: String, magic: MagicCache) -> Result<String, FetchError> {
+    let magic_url = magic.lock().unwrap().get(&url).cloned();
+    let mut attempts = if let Some(magic_url) = magic_url {
+        info!(target: "cache", "Retrieving from magic cache {}", url);
+        match magic_url {
+            CachedMagicUrl::Failed { err, attempts } => {
+                let now = Utc::now();
+                let num_attempts = attempts.len() as u32;
+                warn!(target: "cache", "Failed attempts {}", num_attempts);
+                let timeout = Duration::seconds(2u64.pow(num_attempts - 1) as _);
+                let duration = now.signed_duration_since(attempts.last().unwrap().clone());
+                if duration < timeout {
+                    return Err(err);
+                }
+                attempts
+            }
+            CachedMagicUrl::Success { url, .. } => return Ok(url),
+        }
+    } else {
+        Vec::new()
+    };
+
+    let res = fetch_magic_url(url.clone()).await;
+    info!(target: "cache", "Caching magic {}", url);
+    match res {
+        Ok(magic_url) => {
+            let time = Utc::now();
+            magic.lock().unwrap().insert(
+                url,
+                CachedMagicUrl::Success {
+                    url: magic_url.clone(),
+                    time,
+                },
+            );
+            Ok(magic_url)
+        }
+        Err(err) => {
+            attempts.push(Utc::now());
+            magic.lock().unwrap().insert(
+                url,
+                CachedMagicUrl::Failed {
+                    err: err.clone(),
+                    attempts,
+                },
+            );
+            Err(err)
+        }
+    }
+}
+
 fn cache_and_return(imgs: ImgCache, saved_image: SavedImage) -> Image {
-    let naive = NaiveDateTime::from_timestamp(saved_image.time_nanos / 1_000_000_000, (saved_image.time_nanos % 1_000_000_000) as u32);
+    let naive = NaiveDateTime::from_timestamp(
+        saved_image.time_nanos / 1_000_000_000,
+        (saved_image.time_nanos % 1_000_000_000) as u32,
+    );
     let time = DateTime::from_utc(naive, Utc);
     imgs.lock().unwrap().insert(
         saved_image.pair,
@@ -188,7 +282,10 @@ fn cache_and_return(imgs: ImgCache, saved_image: SavedImage) -> Image {
 async fn fetch_img(url: String) -> Result<Image, FetchError> {
     info!(target: "fetch", "Fetching {}", url);
     let client = reqwest::Client::new();
-    let response = client.get(&url).header(REFERER, env::var_os("REFERER").unwrap().to_str().unwrap()).send()
+    let response = client
+        .get(&url)
+        .header(REFERER, env::var_os("REFERER").unwrap().to_str().unwrap())
+        .send()
         .await
         .map_err(|_e| FetchError::RequestFailed)?;
     if !response.status().is_success() {
@@ -210,6 +307,35 @@ async fn fetch_img(url: String) -> Result<Image, FetchError> {
         content_type,
         body: body.to_vec(),
     })
+}
+
+async fn fetch_magic_url(url: String) -> Result<String, FetchError> {
+    info!(target: "fetch", "Fetching magic url {}", url);
+    let client = reqwest::Client::new();
+    let response = client
+        .get(&url)
+        .header(REFERER, env::var_os("REFERER").unwrap().to_str().unwrap())
+        .send()
+        .await
+        .map_err(|_e| FetchError::RequestFailed)?;
+    if !response.status().is_success() {
+        return Err(FetchError::Status(response.status()));
+    }
+    let content_type = response
+        .headers()
+        .get("content-type")
+        .ok_or_else(|| FetchError::UnsupportedContentType)?
+        .to_str()
+        .map_err(|_e| FetchError::UnsupportedContentType)?
+        .to_string();
+    if !content_type.starts_with("text/plain") {
+        return Err(FetchError::UnsupportedContentType);
+    }
+    let text = response
+        .text()
+        .await
+        .map_err(|_e| FetchError::TextReadFailed)?;
+    Ok(text)
 }
 
 fn read_from_disk(pair: &ImgPair) -> Option<SavedImage> {
@@ -239,7 +365,7 @@ fn write_to_disk(pair: ImgPair, image: Image) -> Result<SavedImage, std::io::Err
     let (dir, path) = pair_to_path(&pair);
     std::fs::create_dir_all(dir)?;
     let mut file = File::create(path).unwrap();
-    let saved_image = SavedImage{
+    let saved_image = SavedImage {
         image,
         pair,
         time_nanos: Utc::now().timestamp_nanos(),
